24134011a40ec876becaa09d4a919358586a7493
sajjadhussains <sajjadhussain14.bd@gmail.com>
2020-09-28 10:25:57 +0600

    second commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('thank you for calling me');
+});
+
+app.listen(4000,()=>console.log('Listening to port 4000'));
